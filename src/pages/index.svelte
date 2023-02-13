21b89e12691754a2dfc93f1a256a5158693fa12f
potatosouuup <85465265+potatosouuup@users.noreply.github.com>
2023-02-13 14:01:37 +0900

    save

---
+++ src/pages/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let count = 0;
+
+	const a = 0;
+
+	const clickHandler = () => {
+		count += 1;
+	};
+</script>
+
+<p>{count}1</p>
+<div>
+	<button on:click="{clickHandler}" class="">button</button>
+</div>
