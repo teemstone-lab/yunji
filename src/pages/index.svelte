3933ba9c76118cabff6924b7275fc47b279f26a5
potatosouuup <85465265+potatosouuup@users.noreply.github.com>
2023-02-14 16:58:33 +0900

    save

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import Nav from '../components/nav/Nav.svelte';
+	// import Nav from '../components/nav/Nav.svelte';
 	let count = 0;
 	const clickHandler = () => {
 		count += 1;
 	};
 </script>
 
-<Nav />
+<!-- <Nav /> -->
 <p>{count}1</p>
 <div>
 	<button on:click="{clickHandler}" class="">button</button>
--- src/pages/test/[slug].svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { params } from '@roxi/routify';
-
-	let slug: string;
-	$: {
-		if (typeof $params.slug === 'string') slug = $params.slug;
-	}
-</script>
