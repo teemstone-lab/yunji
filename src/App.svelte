834156fdc10e390beeb2de58b1ba0766da673025
potatosouuup <85465265+potatosouuup@users.noreply.github.com>
2023-02-23 14:32:00 +0900

    TEST

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
-	// import { Router } from '@roxi/routify';
-	// import { routes } from '../.routify/routes';
+	let currentPage = 'index';
+	import Index from './pages/index.svelte';
+	import Markup1 from './pages/markup/html1.svelte';
+	import Markup2 from './pages/markup/html2.svelte';
+	import Markup3 from './pages/markup/html3.svelte';
+
+	type ClickEvent = MouseEvent & {
+		currentTarget: EventTarget & HTMLButtonElement;
+	};
+	const clickHandler = (e: ClickEvent) => {
+		const target = e.target as HTMLButtonElement;
+		currentPage = target.innerText;
+	};
 </script>
 
-<!-- <Router routes="{routes}" /> -->
+<ul>
+	<li><button on:click="{clickHandler}">markup/markup1</button></li>
+	<li><button on:click="{clickHandler}">markup/markup2</button></li>
+	<li><button on:click="{clickHandler}">markup/markup3</button></li>
+</ul>
+
+<main>
+	{#if currentPage === 'index'}
+		<Index />
+	{/if}
+	{#if currentPage === 'markup/markup1'}
+		<Markup1 />
+	{/if}
+	{#if currentPage === 'markup/markup2'}
+		<Markup2 />
+	{/if}
+	{#if currentPage === 'markup/markup3'}
+		<Markup3 />
+	{/if}
+</main>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	// import Nav from '../components/nav/Nav.svelte';
+	import Nav from '../components/nav/Nav.svelte';
 	let count = 0;
 	const clickHandler = () => {
 		count += 1;
 	};
 </script>
 
-<!-- <Nav /> -->
-<p>{count}1</p>
+<Nav />
+<p>{count}</p>
 <div>
 	<button on:click="{clickHandler}" class="">button</button>
 </div>
