dbb2ed8bf473cdbfb73fcd0283735229f02d7a94
YunjiLeeee <yjlee@ontune.co.kr>
2023-03-31 15:45:12 +0900

    save

--- src/components/carousel/groupWorker.ts
+++ src/components/carousel/groupWorker.ts
@@ -19,7 +19,7 @@ const numberChecker = (num: number) => {
 };
 
 // 웹 워커에서 메시지 받기
-self.onmessage = async (event: MessageEvent<CreateNumberRange | MockGroupType[]>) => {
+onmessage = async (event: MessageEvent<CreateNumberRange | MockGroupType[]>) => {
 	const newGroup = event.data as CreateNumberRange;
 	const minNum = newGroup.min;
 	const maxNum = newGroup.max;
@@ -71,5 +71,5 @@ self.onmessage = async (event: MessageEvent<CreateNumberRange | MockGroupType[]>
 
 	const result = isNewGroupsCase ? await mockGroupsCreator(count, true) : await returnGroupList();
 
-	self.postMessage(result);
+	postMessage(result);
 };
