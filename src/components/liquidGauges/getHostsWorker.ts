719c8c5dfe3f6f5a7f7c10987127e8a7e54c7041
YunjiLeeee <yjlee@ontune.co.kr>
2023-05-16 10:50:43 +0900

    fix: MSW

--- src/components/liquidGauges/getHostsWorker.ts
+++ src/components/liquidGauges/getHostsWorker.ts
@@ -4,11 +4,16 @@ onmessage = function (event: MessageEvent) {
 	let isUpdating: boolean = false;
 
 	const callMsw = () => {
-		fetch(isUpdating ? '/hosts/data' : '/hosts', {
-			headers: {
-				'Content-Type': 'application/json',
+		fetch(
+			isUpdating
+				? 'https://teemstone-lab.github.io/yunji/hosts/data'
+				: 'https://teemstone-lab.github.io/yunji/hosts',
+			{
+				headers: {
+					'Content-Type': 'application/json',
+				},
 			},
-		})
+		)
 			.then((response) => response.json())
 			.then((data) => postMessage(data as HostDataType[]))
 			.catch((error) => console.error(error));
@@ -18,7 +23,7 @@ onmessage = function (event: MessageEvent) {
 		callMsw();
 
 		setTimeout(() => {
-			fetch('/hosts/data', {
+			fetch('https://teemstone-lab.github.io/yunji/hosts/data', {
 				headers: {
 					'Content-Type': 'application/json',
 				},
